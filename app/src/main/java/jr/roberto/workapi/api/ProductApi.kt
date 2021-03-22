4ec661e944526702f6ad1f6386a0882ecae86a79
Roberto Júnior <rbezerra18@hotmail.com>
2021-03-22 12:57:12 -0300

    App inicial. Criar model e api. Criar XML da RecyclerView.

---
+++ app/src/main/java/jr/roberto/workapi/api/ProductApi.kt
@@ -0,0 +1,10 @@
+package jr.roberto.workapi.api
+
+import jr.roberto.workapi.model.Product
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ProductApi {
+    @GET("getdata.php")
+    fun getProductApi(): Call<List<Product>>
+}
